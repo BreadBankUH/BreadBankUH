849cd217a6090c50d1ae49503dfd4264e82567e7
yhanessaanne <ysales@hawaii.edu>
2023-03-28 12:49:09 -1000

    Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,15 +5,16 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+      <Col className="text-center">
+        <Image src="/images/bread-bank-uh-logo.png" width="150px" />
+        <h1>Welcome to Bread Bank UH</h1>
+        <p>Online access to your bank account 24/7</p>
+        <h6>Deposit Checks, Withdraw Money, Transfer Money and more with Online Banking!</h6>
+        <button type="button" className="btn btn-primary btn-lg">Login</button>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <h6>Don't have an account? Sign up today!</h6>
+        <button type="button" className="btn btn-success btn-lg">Sign Up</button>
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
-
     </Row>
   </Container>
 );
